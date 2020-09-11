d946cf8e7bf2e4475a7a4a83b59cc8e4b5480ce1
jkohlen <386329@student.fontys.nl>
2020-09-11 11:09:58 +0200

    cubeMove

---
+++ Assets/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float Speed = 1f;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(transform.position + transform.forward * (Speed * Time.deltaTime));
+    }
+}
