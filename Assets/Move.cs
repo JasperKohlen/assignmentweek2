9997555b1d10445c4cb4dbb7d68bd5fa9746124e
jkohlen <386329@student.fontys.nl>
2020-09-18 09:42:01 +0200

    Feedback toegepast

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -6,6 +6,8 @@ public class Move : MonoBehaviour
 {
     public float Speed = 1f;
     private Rigidbody rigidbody;
+    private bool dragging = false;
+    private float distance;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +15,36 @@ public class Move : MonoBehaviour
     }
 
     // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 v = transform.position * Speed * Time.deltaTime;
+            rigidbody.velocity = v;
+            
+            //rigidbody.AddForce(transform.forward * Speed);
+
+        }
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 v = transform.forward * Speed;
-            rigidbody.velocity = v;
+            distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+            dragging = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+        }
+
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = new Vector3(transform.position.x, rayPoint.y, rayPoint.z);
         }
     }
 }
